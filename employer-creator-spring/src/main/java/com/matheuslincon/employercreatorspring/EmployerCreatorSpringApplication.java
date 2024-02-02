b836786d0ec6cb541d47123ca6f446fc9a270f21
Matheus Andrade <matheuslincon50@gmail.com>
2024-02-02 16:03:53 +1100

    chore: initialize spring app

---
+++ employer-creator-spring/src/main/java/com/matheuslincon/employercreatorspring/EmployerCreatorSpringApplication.java
@@ -0,0 +1,13 @@
+package com.matheuslincon.employercreatorspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployerCreatorSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployerCreatorSpringApplication.class, args);
+	}
+
+}
