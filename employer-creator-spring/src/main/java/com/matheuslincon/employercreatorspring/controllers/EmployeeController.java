0ede5465765d643d4544cb8653b04874106b922d
Matheus Andrade <matheuslincon50@gmail.com>
2024-02-08 20:45:36 +1100

    feat: implemented rest of crud operations, respectives DTOs

--- employer-creator-spring/src/main/java/com/matheuslincon/employercreatorspring/controllers/EmployeeController.java
+++ employer-creator-spring/src/main/java/com/matheuslincon/employercreatorspring/controllers/EmployeeController.java
@@ -1,8 +1,13 @@
 package com.matheuslincon.employercreatorspring.controllers;
 
+import com.matheuslincon.employercreatorspring.data.dto.EmployeeCreateDTO;
 import com.matheuslincon.employercreatorspring.data.dto.EmployeeDTO;
+import com.matheuslincon.employercreatorspring.data.dto.EmployeeUpdateDTO;
 import com.matheuslincon.employercreatorspring.services.EmployeeService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,12 +20,28 @@ public class EmployeeController {
     private EmployeeService service;
 
     @GetMapping
-    public List<EmployeeDTO> findAll() {
-        return service.findAll();
+    public ResponseEntity<List<EmployeeDTO>> findAll() {
+        return new ResponseEntity<>(this.service.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeDTO> findById(@PathVariable Long id) {
+        return new ResponseEntity<>(this.service.findById(id), HttpStatus.OK);
     }
 
     @PostMapping
-    public EmployeeDTO create(@RequestBody EmployeeDTO employee) {
-        return service.create(employee);
+    public ResponseEntity<EmployeeDTO> create(@Valid @RequestBody EmployeeCreateDTO data) {
+        return new ResponseEntity<>(this.service.create(data), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EmployeeDTO> update(@PathVariable Long id, @Valid @RequestBody EmployeeUpdateDTO data) {
+        return new ResponseEntity<>(this.service.update(id, data), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- employer-creator-spring/src/main/java/com/matheuslincon/employercreatorspring/services/EmployeeService.java
+++ employer-creator-spring/src/main/java/com/matheuslincon/employercreatorspring/services/EmployeeService.java
@@ -1,6 +1,9 @@
 package com.matheuslincon.employercreatorspring.services;
 
+import com.matheuslincon.employercreatorspring.data.dto.EmployeeCreateDTO;
 import com.matheuslincon.employercreatorspring.data.dto.EmployeeDTO;
+import com.matheuslincon.employercreatorspring.data.dto.EmployeeUpdateDTO;
+import com.matheuslincon.employercreatorspring.exceptions.ResourceNotFoundException;
 import com.matheuslincon.employercreatorspring.model.Employee;
 import com.matheuslincon.employercreatorspring.repositories.EmployeeRepository;
 import org.modelmapper.ModelMapper;
@@ -26,10 +29,37 @@ public class EmployeeService {
                 .collect(Collectors.toList());
     }
 
-    public EmployeeDTO create(EmployeeDTO employee) {
+    public EmployeeDTO findById(Long id) {
+        Employee employee = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID !"));
+        return mapper.map(employee, EmployeeDTO.class);
+    }
 
-        Employee newEmployee = mapper.map(employee, Employee.class);
+    public EmployeeDTO create(EmployeeCreateDTO data) {
+
+        Employee newEmployee = mapper.map(data, Employee.class);
         Employee createdEmployee = repository.save(newEmployee);
         return mapper.map(createdEmployee, EmployeeDTO.class);
+    }
+
+    public EmployeeDTO update(Long id, EmployeeUpdateDTO data) {
+        Employee employee = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID !"));
+
+        employee.setFirstName(data.getFirstName());
+        employee.setMiddleName(data.getMiddleName());
+        employee.setLastName(data.getLastName());
+        employee.setMobile(data.getMobile());
+        employee.setAddress(data.getAddress());
+        employee.setContractType(data.getContractType());
+        employee.setStartDate(data.getStartDate());
+        employee.setFinishDate(data.getFinishDate());
+        employee.setHoursType(data.getHoursType());
+        employee.setHoursPerWeek(data.getHoursPerWeek());
+
+        return mapper.map(this.repository.save(employee), EmployeeDTO.class);
+    }
+
+    public void delete(Long id) {
+        Employee employee = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID !"));
+        repository.delete(employee);
     }
 }
