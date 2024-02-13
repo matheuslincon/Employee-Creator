4ffdf3c99ad9f91884928d1775a591c8bbc66a07
Matheus Andrade <matheuslincon50@gmail.com>
2024-02-13 15:48:52 +1100

    feat: config swagger on employee endpoint

--- employer-creator-spring/src/main/java/com/matheuslincon/employercreatorspring/controllers/EmployeeController.java
+++ employer-creator-spring/src/main/java/com/matheuslincon/employercreatorspring/controllers/EmployeeController.java
@@ -4,6 +4,12 @@ import com.matheuslincon.employercreatorspring.data.dto.EmployeeCreateDTO;
 import com.matheuslincon.employercreatorspring.data.dto.EmployeeDTO;
 import com.matheuslincon.employercreatorspring.data.dto.EmployeeUpdateDTO;
 import com.matheuslincon.employercreatorspring.services.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,32 +20,78 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
+@Tag(name = "Employees", description = "Endpoints for managing employees")
 public class EmployeeController {
 
     @Autowired
     private EmployeeService service;
 
     @GetMapping
+    @Operation(summary = "Finds all employees", description = "Finds all employees", tags = {"Employees"}, responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = {
+                    @Content(mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = EmployeeDTO.class))
+                    )
+            }),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+    })
     public ResponseEntity<List<EmployeeDTO>> findAll() {
         return new ResponseEntity<>(this.service.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Finds an employee", description = "Finds an employee", tags = {"Employees"}, responses = {
+            @ApiResponse(description = "Success", responseCode = "200",
+                        content = @Content(schema = @Schema(implementation = EmployeeDTO.class))
+            ),
+            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+    })
     public ResponseEntity<EmployeeDTO> findById(@PathVariable Long id) {
         return new ResponseEntity<>(this.service.findById(id), HttpStatus.OK);
     }
 
     @PostMapping
+    @Operation(summary = "Adds a new employee", description = "Adds a new employee", tags = {"Employees"}, responses = {
+            @ApiResponse(description = "Success", responseCode = "200",
+                    content = @Content(schema = @Schema(implementation = EmployeeDTO.class))
+            ),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+    })
     public ResponseEntity<EmployeeDTO> create(@Valid @RequestBody EmployeeCreateDTO data) {
         return new ResponseEntity<>(this.service.create(data), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Updates an employee", description = "Updates an employee", tags = {"Employees"}, responses = {
+            @ApiResponse(description = "Updated", responseCode = "200",
+                    content = @Content(schema = @Schema(implementation = EmployeeDTO.class))
+            ),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+    })
     public ResponseEntity<EmployeeDTO> update(@PathVariable Long id, @Valid @RequestBody EmployeeUpdateDTO data) {
         return new ResponseEntity<>(this.service.update(id, data), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deletes an employee", description = "Deletes an employee", tags = {"Employees"}, responses = {
+            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+    })
     public ResponseEntity<?> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
