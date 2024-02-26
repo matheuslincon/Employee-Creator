524b2ac171e239e7218019ab1dbbb9a651b44d21
Matheus Andrade <matheuslincon50@gmail.com>
2024-02-26 17:01:12 +1100

    chore: install react-router-dom

--- Frontend/employer-creator-react/src/App.tsx
+++ Frontend/employer-creator-react/src/App.tsx
@@ -1,9 +1,13 @@
 import './global.css'
 
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './routes'
+
 export function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <RouterProvider router={router} />
     </>
   )
 }
