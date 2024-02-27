7932dd07fafcc1b3173dbed0ed0af4988275332d
Matheus Andrade <matheuslincon50@gmail.com>
2024-02-27 15:11:27 +1100

    feat: design auth layout

--- Frontend/employer-creator-react/src/pages/_layouts/auth.tsx
+++ Frontend/employer-creator-react/src/pages/_layouts/auth.tsx
@@ -1,11 +1,19 @@
+import { Building2 } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
-export function AuthLayout() {
+export const AuthLayout = () => {
   return (
-    <div>
-      <h1>Auth</h1>
-
-      <div>
+    <div className="grid min-h-screen grid-cols-2 antialiased">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <div className="flex items-center gap-3 text-lg text-foreground">
+          <Building2 className="h-5 w-5" />
+          <span className="font-semibold">Employers Creator</span>
+        </div>
+        <footer className="text-sm">
+          Matheus panel &copy; Employers Creator - {new Date().getFullYear()}
+        </footer>
+      </div>
+      <div className="relative flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
