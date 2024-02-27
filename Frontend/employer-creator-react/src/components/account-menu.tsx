bd8a8360349e034f819e9396cca95c2dad9fa61a
Matheus Andrade <matheuslincon50@gmail.com>
2024-02-27 18:52:23 +1100

    feat: design app layout

---
+++ Frontend/employer-creator-react/src/components/account-menu.tsx
@@ -0,0 +1,45 @@
+import { ChevronDown, LogOut, User } from 'lucide-react'
+
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+export const AccountMenu = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex select-none items-center gap-2"
+        >
+          Employers Creator
+          <ChevronDown className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Matheus Andrade</span>
+          <span className="text-xs font-normal text-muted-foreground">
+            matheuslincon@email.com
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <User className="mr-2 h-4 w-4" />
+          <span>Profile</span>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Log Out</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
---
+++ Frontend/employer-creator-react/src/components/header.tsx
@@ -0,0 +1,30 @@
+import { Factory, Home } from 'lucide-react'
+
+import { AccountMenu } from './account-menu'
+import { NavLink } from './nav-link'
+import { ModeToggle } from './theme/theme-toggle'
+import { Separator } from './ui/separator'
+
+export const Header = () => {
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center gap-6 px-6">
+        <Factory className="h-6 w-6" />
+
+        <Separator orientation="vertical" className="h-6" />
+
+        <nav className="flex items-center space-x-4 lg:space-x-6">
+          <NavLink to="/">
+            <Home className="h-4 w-4" />
+            Home
+          </NavLink>
+        </nav>
+
+        <div className="ml-auto flex items-center gap-2">
+          <ModeToggle />
+          <AccountMenu />
+        </div>
+      </div>
+    </div>
+  )
+}
